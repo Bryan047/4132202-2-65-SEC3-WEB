087d5689d7f8113e1a714a90c994df33656888b2
Bryan047 <sorrybro1125@gmail.com>
2023-03-07 02:00:51 +0700

    ff

--- web/main2.php
+++ web/main2.php
@@ -45,35 +45,35 @@ if (isset($_SESSION['id']) && isset($_SESSION['user_name'])){
         <h2 id="2" class="sub-title">สถานที่ท่องเที่ยว</h2>
         <div class="exclusive">
             <div>
-                <img src="../taveling/ChiangMai/13.png" >
+            <img src="https://upload.wikimedia.org/wikipedia/commons/thumb/c/c6/At_the_Top.jpg/1200px-At_the_Top.jpg "  >
                 <span>
                     <h3>Chiang Mai</h3>
                     
                 </span>
             </div>
             <div>
-                <img src="../taveling/ChiangRai/12.png" >
+                <img src="https://s359.kapook.com/pagebuilder/f1cad28c-eb5b-4ca4-9551-2f6f40026171.jpg" >
                 <span>
                     <h3>Chiang Rai</h3>
                     
                 </span>
             </div>
             <div>
-                <img src="../taveling/luopang/111.png" >
+                <img src="https://img.wongnai.com/p/800x0/2020/10/16/2a3c98008a8b4432ba00019164316fbd.jpg" >
                 <span>
                     <h3>Lampang</h3>
                    
                 </span>
             </div>
             <div>
-                <img src="../taveling/MaeHongSon/14.png" >
+                <img src="https://www.maehongsongreentravel.com/images/content/original-1649754083916.jpg" >
                 <span>
                     <h3>Mae Hong Son</h3>
                     
                 </span>
             </div>
             <div>
-                <img src="../taveling/nan/15.png" >
+                <img src="https://gangtravel.com/upload/2020/07/15_900x600-5.jpg.webp" >
                 <span>
                     <h3>Nan</h3>
                     
@@ -86,19 +86,19 @@ if (isset($_SESSION['id']) && isset($_SESSION['user_name'])){
         <h2 id="1" class="sub-title">Cafe</h2>
         <div class="trending">
             <div>
-                <img src="../cafe/Groon/1.jpg" >
+                <img src="https://lh3.googleusercontent.com/p/AF1QipPzyGyec9ZVNnm-50ik90fBo6Ttm6si00x7d0Hk=s680-w680-h510" >
                 <h3>Groon Cafe</h3>
             </div>
             <div>
-                <img src="../cafe/nakama cafe/1.jpg" >
+                <img src="https://lh3.googleusercontent.com/p/AF1QipNUvvjYbh4R1Mdt7b-OJZwRxUq1d1_5humOUfkt=s680-w680-h510" >
                 <h3>Nakama Cafe</h3>
             </div>
             <div>
-                <img src="../cafe/som cafe/1.jpg" >
+                <img src="https://lh3.googleusercontent.com/p/AF1QipMapl_LziGCCOZQ7-OATAPwTMhE4rrb4ByI0AWD=s680-w680-h510" >
                 <h3>ชมวิวค่าเฟ่ </h3>
             </div>
             <div>
-                <img src="../cafe/Yelloo/1.jpg" >
+                <img src="https://www.kongaroi.com/wp-content/uploads/2020/12/yelloo.jpg" >
                 <h3>Yelloo cafe</h3>
             </div>       
         </div>
@@ -106,15 +106,15 @@ if (isset($_SESSION['id']) && isset($_SESSION['user_name'])){
         <h2 id="3" class="sub-title">สถานบันเทิง</h2>
         <div class="stories">
             <div>
-                <img src="../taveling/ChiangMai/bar/1.png">
+                <img src="https://scontent.fnak3-1.fna.fbcdn.net/v/t1.18169-9/14718799_1178347208869596_1126570092056692928_n.jpg?_nc_cat=100&ccb=1-7&_nc_sid=174925&_nc_eui2=AeGlXW_MOQ_uAQmEGXTHbwTmHE3HnrRZFWAcTceetFkVYDSYoQiURe1tS8BR-loJsMcjWmY_I51Exm1mnHVfTxop&_nc_ohc=9ugIwYod1zYAX99jxkg&_nc_ht=scontent.fnak3-1.fna&oh=00_AfBzvkWQXOPptTHu8KSLFTtI6QFWF7ZzVWt-zfDp0Bgsig&oe=642DBD67">
                 <p>Warmup Cafe ผับดังของเชียงใหม่</p>
             </div>
             <div>
-                <img src="../taveling/ChiangRai/bar/1.png">
+                <img src="https://scontent.fnak3-1.fna.fbcdn.net/v/t39.30808-6/289246748_110210895059301_794146946500670141_n.jpg?_nc_cat=105&ccb=1-7&_nc_sid=09cbfe&_nc_eui2=AeGknCMDcKIUPK_qQTSzQri2iY0KW_vuxDyJjQpb--7EPH12_H-u5X1iSDqo4alba3egu7-Kx0LadX9BSnlHE7OB&_nc_ohc=h2xws-aD0C0AX_ZYBau&_nc_ht=scontent.fnak3-1.fna&oh=00_AfAGWw10mP7SZXcs3_37O55dsiOQ82o8g-IvhZQoaV-KfA&oe=640A8B47">
                 <p>Ribbon Bar จังหวัดเชียงราย</p>
             </div>
             <div>
-                <img src="../taveling/MaeHongSon/bar/13.png">
+                <img src="https://scontent.fnak3-1.fna.fbcdn.net/v/t39.30808-6/277782100_630647131618522_1770453429291049892_n.jpg?_nc_cat=106&ccb=1-7&_nc_sid=09cbfe&_nc_eui2=AeEjFbwGx98P8O-MlTJrAewMib_KH9fygFiJv8of1_KAWCa0pulU6zMVP8XpwBFwri5vaQKj0Kb_3y1UxcpZVRLs&_nc_ohc=bgCRKva2SykAX8ZxgKN&_nc_ht=scontent.fnak3-1.fna&oh=00_AfB7nMvowBkqhEyST-dF796yztXSdhsTWA6xVzSNYl0eNw&oe=640BD481">
                 <p>จิ๊กโก๋บาร์ แท็ปเบียร์ ซิงเกิลมอลต์ จังหวัดแม่ฮ่องสอน</p>
             </div>
         </div>
